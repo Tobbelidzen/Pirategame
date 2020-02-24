7a320d9c76fa479a199a127ef02c260fe81f0856
pognar <senseicreep@gmail.com>
2020-02-24 15:26:44 +0100

    first commit #button

---
+++ Area2D.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click
+
+#ange funktion av knapp här
+func on_click():
+	print("ehcek")
+		

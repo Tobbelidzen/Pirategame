4b5867175ca5e15bbd31187a52350e342a44a27f
pognar <senseicreep@gmail.com>
2020-03-02 14:37:08 +0100

    primary safety commit #progress on working doors

--- scripts/enterdoor.gd
+++ scripts/enterdoor.gd
@@ -1,15 +1,11 @@
 extends Area2D
 
-onready var playerenter = get_node("res://player.tscn")
-var player_enter = false
+onready var player = get_node("/root/bootybae/player")
 
+func _ready():
+	connect("body_entered",self,"body_entered")
+	player.speed=100
 
-#func _ready():
-    #connect("body_enter", self, "n_enemy_body_enter")
-
-
-func _on_Area2D_body_entered(body):
-	if "PlayerBody" in body.name:
-		player_enter = true
-	if player_enter == true:
-		print("whacky")
+func body_entered(body):
+	if body.name == "player" && Input.is_action_pressed('up'):
+		print("treterktoeorj")
--- scripts/movement.gd
+++ scripts/movement.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export (int) var speed = 50
 
 var velocity = Vector2()
-
+var upkey = 0
 func get_input():
     velocity = Vector2()
     if Input.is_action_pressed('ui_right'):
@@ -11,11 +11,21 @@ func get_input():
     if Input.is_action_pressed('ui_left'):
         velocity.x -= 1
     if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
+        velocity.y += 0
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 0
+        upkey = 1
     velocity = velocity.normalized() * speed
+    #print(speed)
 
 func _physics_process(delta):
     get_input()
     velocity = move_and_slide(velocity)
+	
+
+
+func _on_Doorscene_body_entered(body):
+	if upkey > 0:
+		print("yALLA2")
+
+
